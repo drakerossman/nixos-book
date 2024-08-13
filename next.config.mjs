3fc01818c089ac327bf32ae076bffdde23957eb1
Drake Rossman <drake.rossman@protonmail.com>
2024-08-13 10:05:04 -0400

    add headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,56 @@
 /** @type {import('next').NextConfig} */
+
+
+// You might need to insert additional domains in script-src if you are using external services
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.goatcounter.com *.zgo.at mastodon.social;
+  style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
+  img-src * blob: data:;
+  media-src nixosbook.com localhost localhost:3000;
+  connect-src *;
+  font-src 'self' fonts.gstatic.com cdn.jsdelivr.net;
+  frame-src giscus.app *.goatcounter.com *.zgo.at mastodon.social
+`
+
+const securityHeaders = [
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+    {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\n/g, ''),
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+    {
+        key: 'Referrer-Policy',
+        value: 'strict-origin-when-cross-origin',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+    {
+        key: 'X-Frame-Options',
+        value: 'DENY',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=()',
+    },
+]
+
 const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
@@ -6,3 +58,14 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ]
+    },
+}
