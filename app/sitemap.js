9d2be895e44ff36f6a288b3bd32b2f8ea8bd38a7
Drake Rossman <drake.rossman@protonmail.com>
2024-08-13 09:55:52 -0400

    add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://nixosbook.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    }
+  ]
+}
